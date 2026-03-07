876a49da3756488f6cfda6beda0c6796f68252e7
JunWoo Seo <csjws@naver.com>
2026-03-07 15:24:40 +0900

    2-16 구현

--- src/main/java/com/back/answer/AnswerController.java
+++ src/main/java/com/back/answer/AnswerController.java
@@ -2,13 +2,14 @@ package com.back.answer;
 
 import com.back.question.Question;
 import com.back.question.QuestionService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
@@ -19,9 +20,13 @@ public class AnswerController {
     private final AnswerService answerService;
 
     @PostMapping("/create/{id}")
-    public String createAnswer(Model model, @PathVariable("id") int id, @RequestParam(value="content") String content) {
+    public String createAnswer(Model model, @PathVariable("id") int id, @Valid AnswerForm answerForm, BindingResult bindingResult) {
         Question question = questionService.getQuestion(id);
-        answerService.create(question, content);
-        return String.format("redirect:/question/detail/%s", id);
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("question", question);
+            return "question_detail";
+        }
+        answerService.create(question, answerForm.getContent());
+        return "redirect:/question/detail/%s".formatted(id);
     }
 }
--- src/main/java/com/back/question/QuestionService.java
+++ src/main/java/com/back/question/QuestionService.java
@@ -4,6 +4,7 @@ import com.back.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -17,12 +18,20 @@ public class QuestionService {
         return questionRepository.findAll();
     }
 
-    public Question getQuestion(Integer id) {
+    public Question getQuestion(int id) {
         Optional<Question> question = questionRepository.findById(id);
         if (question.isPresent()) {
             return question.get();
         } else {
             throw new DataNotFoundException("question not found");
         }
+    }
+
+    public void create(String subject, String content) {
+        Question q = new Question();
+        q.setSubject(subject);
+        q.setContent(content);
+        q.setCreateDate(LocalDateTime.now());
+        questionRepository.save(q);
     }
 }
