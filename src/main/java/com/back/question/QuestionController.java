94aae0af40d2559e619e5b6a8110500c7a5adce4
JunWoo Seo <csjws@naver.com>
2026-03-09 17:35:24 +0900

    3-02 구현

--- src/main/java/com/back/question/QuestionController.java
+++ src/main/java/com/back/question/QuestionController.java
@@ -3,15 +3,11 @@ package com.back.question;
 import com.back.answer.AnswerForm;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.*;
 
 
 @Controller
@@ -22,9 +18,9 @@ public class QuestionController {
     private final QuestionService questionService;
 
     @GetMapping("/list")
-    public String list(Model model) {
-        List<Question> questionList = questionService.getList();
-        model.addAttribute("questionList", questionList);
+    public String list(@RequestParam(value="page", defaultValue="0") int page, Model model) {
+        Page<Question> paging = questionService.getList(page);
+        model.addAttribute("paging", paging);
         return "question_list";
     }
 
