8fdc8ee08f1dd98f29bf04578d627b3c97738369
JunWoo Seo <csjws@naver.com>
2026-03-17 16:16:48 +0900

    3-09 구현

--- src/main/java/com/back/answer/AnswerService.java
+++ src/main/java/com/back/answer/AnswerService.java
@@ -1,11 +1,13 @@
 package com.back.answer;
 
+import com.back.DataNotFoundException;
 import com.back.question.Question;
 import com.back.user.SiteUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -21,5 +23,23 @@ public class AnswerService {
         answer.setAuthor(author);
         answerRepository.save(answer);
         return answer;
+    }
+
+    public Answer getAnswer(int id) {
+        Optional<Answer> answer = answerRepository.findById(id);
+        if (answer.isPresent()) {
+            return answer.get();
+        } else {
+            throw new DataNotFoundException("answer not found");
+        }
+    }
+    public void modify(Answer answer, String content) {
+        answer.setContent(content);
+        answer.setModifyDate(LocalDateTime.now());
+        answerRepository.save(answer);
+    }
+
+    public void delete(Answer answer) {
+        answerRepository.delete(answer);
     }
 }
